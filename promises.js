3c5233664c0096f72948e09d2f97a5064130771c
Nix-code <nishantbanjade07@gmail.com>
2023-03-02 09:39:36 +0530

    add promises in javascript

---
+++ promises.js
@@ -0,0 +1,27 @@
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
+
+ let is_shop_open = true;
+
+ let order = (time, work) => {
+    
+    return new Promise((resolve, reject) => {
+        if(is_shop_open){
+            
+            setTimeout(()=>{
+                resolve(work());
+            }, time);
+            
+        }
+        else{
+
+            reject(console.log("our shop is closed"))
+        }
+    });
+ };
+
+ order(2000, ()=>console.log(`${stocks.Fruits[0]}`));
