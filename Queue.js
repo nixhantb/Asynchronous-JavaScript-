0632207a137877d631896fbc27b45050125805b9
Nix-code <nishantbanjade07@gmail.com>
2023-03-08 00:50:29 +0530

    add xhr

---
+++ Queue.js
@@ -0,0 +1,11 @@
+
+console.log("Getting Bus ticket")
+const personFirst = "I am in a queue for 1 hour and I will get the ticket first.";
+const personSecond = "I will use mobile banking and internet to buy a ticket"
+
+setTimeout(()=>{
+    console.log("I am the first person")
+    console.log("I am still in a queue.")
+},4000);
+
+console.log("I am the second person and I got my ticket in just few minutes.");
